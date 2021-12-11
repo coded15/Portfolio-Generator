f8ea5510ab40a974a0952d640917fa6424d7d121
coded15 <91874598+coded15@users.noreply.github.com>
2021-12-11 15:38:35 +0530

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,12 +13,68 @@ import Header from "./Bootstrap/Header";
 //   );
 // };
 class App extends Component {
+  state = {
+    Dark: false,
+    Name: "",
+  };
+  toggleHeader = () => {
+    // window.alert("Toggle Header Clicked");
+    this.setState({
+      Dark: !this.state.Dark,
+    });
+  };
+  changeHandler = e => {
+    this.setState({
+      Name: e.target.value,
+    });
+  };
   render() {
     return (
       <div className="App">
-        <Header>Portfolio Generator</Header>
+        <Header dark={this.state.Dark} className="mb-5">
+          Portfolio Generator Light
+        </Header>
+        {/* <Header dark={true}>Portfolio Generator Dark</Header> */}
         {/* <h1>Hello Scaler</h1> */}
         {/* <ArrowHeader Hello="World" Name="Ichchha Gupta"></ArrowHeader> */}
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center mb-3">
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.Name}
+                onChange={this.changeHandler}
+              />
+            </div>
+            <div className="col-12 text-center mb-3">
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.Name}
+                onChange={this.changeHandler}
+              />
+            </div>
+            <div className="col-12 text-center">
+              <button
+                className={`btn btn${
+                  this.state.Dark ? "-outline" : ""
+                }-primary`}
+                onClick={this.toggleHeader}
+              >
+                Toggle Dark Mode
+              </button>
+              <button
+                className={`ml-4 btn btn${
+                  this.state.Dark ? "-outline" : ""
+                }-primary`}
+                onClick={this.toggleHeader}
+              >
+                Toggle Dark Mode
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
