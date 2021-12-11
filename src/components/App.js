d7f2b4dc1e288f0c9e2f4c42058c369bc6740f70
coded15 <91874598+coded15@users.noreply.github.com>
2021-12-11 14:05:37 +0530

    Implemented a Bootstrap Heaader Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import Header from "./Bootstrap/Header";
 
+// const ArrowHeader = props => {
+//   return (
+//     <>
+//       <p>
+//         Hello, I am from arrow function, and we have {props.Name} in the Live
+//         Stream.
+//       </p>
+//       <pre>{JSON.stringify(props, null, 2)}</pre>
+//     </>
+//   );
+// };
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Scaler</h1>
+        <Header>Portfolio Generator</Header>
+        {/* <h1>Hello Scaler</h1> */}
+        {/* <ArrowHeader Hello="World" Name="Ichchha Gupta"></ArrowHeader> */}
       </div>
     );
   }
